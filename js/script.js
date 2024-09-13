4930f47c904075752a6187c7cd0e026bb0b172f1
Mubeen Bankur <mobee33@hotmail.com>
2024-09-13 09:10:50 +0400

    changes

--- js/script.js
+++ js/script.js
@@ -1,30 +1,28 @@
-const swipper = new Swiper('.home-slider', {
+const swiper = new Swiper('.home-slider', {
   grabCursor:true,
   loop:true,
   centeredSlides:true,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
-
   }
 });
 
 const navbar = document.querySelector('.header .navbar');
 const cart = document.querySelector('.cart-items-container');
 const menu = document.querySelector('#menu-btn');
-const closeform = document.querySelector('#close-form');
-const cartbtn = document.querySelector('#cart-btn');
+const closeForm = document.querySelector('#close-form');
+const cartBtn = document.querySelector('#cart-btn');
 
-cartbtn.onclick = () =>{
-  cart.classList.add('active')
-}
+cartBtn.onclick = () =>{
+  cart.classList.add('active');
+};
 
-closeform.onclick = () =>{
-  cart.classList.remove('active')
-}
-
+closeForm.onclick = () =>{
+  cart.classList.remove('active');
+};
 
 menu.onclick = () =>{
   menu.classList.toggle('fa-times');
   navbar.classList.toggle('active');
-}
+};
